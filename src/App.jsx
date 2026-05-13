6f329b6e7463fead16a3b97f8bb8f9edd4b8f212
Juan Santander <santanderruizjuancamilo@gmail.com>
2026-05-12 19:04:37 -0500

    soluccion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import { AuthProvider, useAuth } from "./components/context/AuthContext";
-import Home from "./components/Home";
-import Login from "./components/Login_inicio_sesion";
+import { AuthProvider, useAuth } from './components/context/AuthContext'
+import Home from './components/Home'
+import Login from './components/Login_inicio_sesion'
+import Navbar from './components/layout/NavBar'
 
 function AppContent() {
-  const { isAuthenticated, login } = useAuth();
+  const { isAuthenticated, login } = useAuth()
 
   return (
     <div className="app-shell">
+      {/* 1. Ponemos el Navbar aquí para que se vea siempre */}
+      <Navbar />
+
+      {/* 2. El contenido cambia según el login */}
       {isAuthenticated ? <Home /> : <Login onLogin={login} />}
     </div>
-  );
+  )
 }
 
+// SOLO UNA FUNCIÓN APP
 function App() {
   return (
     <AuthProvider>
       <AppContent />
     </AuthProvider>
-  );
-import Navbar from './components/layout/NavBar'
-
-function App() {
-  return (
-    <>
-      <Navbar />
-    </>
   )
 }
 
-export default App;
+export default App
